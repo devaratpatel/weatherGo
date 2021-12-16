203971ddc16d5110c436148b3a5cebd6aee9c566
Devarat Patel <devaratpatel1@gmail.com>
2021-12-16 02:26:31 -0500

    fetch open weather api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import react from 'react';
 import './App.css';
+import Header from './components/Header';
+import { useState } from 'react';
 
 function App() {
+  const apiKey = '31e5249b7f5ee68a30375552ce0232f3';
+  const [weatherData, setWeatherData] = useState([{}]);
+  const [city, setCity] = useState('');
+
+  const getWeather = event => {
+    if (event.key === 'Enter') {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=${apiKey}`
+      )
+        .then(response => response.json())
+        .then(data => {
+          setWeatherData(data);
+          setCity('');
+        });
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <div className="container">
+        <input
+          className="input"
+          placeholder="Enter City..."
+          onChange={event => setCity(event.target.value)}
+          value={city}
+          onKeyPress={getWeather}
+        ></input>
+        {typeof weatherData.main === 'undefined' ? (
+          <div>
+            <p>Welcome to weather app! Enter in a city to get the weather of.</p>
+          </div>
+        ) : (
+          <div className="weather-data">
+            <p className="city">{weatherData.name}</p>
+            <p className="temp">{Math.round(weatherData.main.temp)} °F</p>
+            <p className="weather">{weatherData.weather[0].main}</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
